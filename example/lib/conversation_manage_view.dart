e7cc3ce112f329e2dc518e7f4d6ad14bfd388731
raoxudong <raoxd@jpush.cn>
2019-08-05 15:53:44 +0800

    v0.2.0,新增消息撤回类型，修改 bug

---
+++ example/lib/conversation_manage_view.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:jmessage_flutter/jmessage_flutter.dart';
+import 'package:jmessage_flutter_example/main.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+class ConversationManageView extends StatefulWidget {
+  @override
+  _ConversationManageViewState createState() => _ConversationManageViewState();
+}
+
+class _ConversationManageViewState extends State<ConversationManageView> {
+
+  List<JMConversationInfo> dataList = [];
+  final _commonFont = const TextStyle(fontSize: 18.0);
+  bool _loading = false;
+  String _result = "请选择需要操作的会话";
+  int selectIndex = -1;
+  JMConversationInfo selectConversationInfo;
+
+  @override
+  void initState() {
+    super.initState();
+
+    demoGetConversationList();
+
+  }
+
+  void addMessageEvent() async {
+    jmessage.addReceiveMessageListener((msg) {//+
+      print('listener receive event - message ： ${msg.toJson()}');
+
+      verifyMessage(msg);
+
+      setState(() {
+        _result = "【收到消息】${msg.toJson()}";
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text("会话列表",style: TextStyle(fontSize: 20)),
+        leading: IconButton(
+            icon: new Image.asset("assets/nav_close.png"),
+            onPressed: (){
+              Navigator.maybePop(context);
+            }
+        ),
+      ),
+      body: ModalProgressHUD(inAsyncCall: _loading, child: _buildContentView()),
+    );
+  }
+
+  Widget _buildContentView() {
+    return new Column(
+      children: <Widget>[
+        new Container(
+          margin: EdgeInsets.fromLTRB(5, 5, 5, 0),
+          color: Colors.brown,
+          child: Text(_result),
+          width: double.infinity,
+          height: 120,
+        ),
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            new Text(" "),
+            new CustomButton(title: "发送文本消息", onPressed: demoSendTextMessage),
+            new Text(" "),
+            new CustomButton(title: "获取历史消息", onPressed: demoGetHistorMessage),
+            new Text(" "),
+            new CustomButton(title: "刷新会话列表", onPressed: (){
+              demoGetConversationList();
+            }),
+          ],
+        ),
+        Expanded(
+            child: new Container(
+              color: Colors.grey,
+              margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+              child: new ListView.builder(
+                  itemCount: dataList.length*2,
+                  itemBuilder: (context, i){
+                    if (i.isOdd) return new Divider();
+
+                    final index = i ~/2;
+
+                    return _buildRow(dataList[index], index);
+                  }
+              ),
+            ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildRow(JMConversationInfo object, int index) {
+
+    String title = "【${getStringFromEnum(object.conversationType)}】${object.title}";
+
+    return new Container(
+      height: 60,
+      child: new ListTile(
+        //dense: true,
+        //leading: CircleAvatar(backgroundImage: NetworkImage(url)),
+        //trailing: Icon(Icons.keyboard_arrow_right),
+        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),//设置内容边距，默认是 16
+        title: new Text(title),
+        subtitle: new Text(object.latestMessage.toString()),
+        selected: selectIndex==index,
+        onTap: () {
+          print("点击了第【${index+1}】行");
+          setState(() {
+            selectIndex = index;
+            _result = "【选择的会话】\n ${object.toJson()}";
+          });
+          selectConversationInfo = object;
+        },
+      ),
+    );
+  }
+
+  /// 获取公开群列表
+  void demoGetConversationList () async {
+    print("demoGetConversationList");
+    setState(() {
+      selectIndex = -1;
+      selectConversationInfo = null;
+      _result = "请选择需要操作的会话";
+      _loading = true;
+    });
+
+    List<JMConversationInfo> conversations = await jmessage.getConversations();
+
+    setState(() {
+      _loading = false;
+      dataList = conversations;
+    });
+
+    for (JMConversationInfo info in conversations) {
+      print('会话：${info.toJson()}');
+    }
+  }
+
+  int textIndex = 0;
+  void demoSendTextMessage() async {
+    print("demoSendTextMessage" );
+
+    if (selectConversationInfo == null) {
+      setState(() {
+        _result = "请选着需要操作的会话";
+      });
+      return;
+    }
+    setState(() {
+      _loading = true;
+    });
+
+    JMTextMessage msg = await selectConversationInfo.sendTextMessage(text:  "send msg queen index $textIndex");
+    setState(() {
+      _loading = false;
+      _result = "【文本消息】${msg.toJson()}";
+    });
+    textIndex ++;
+  }
+
+  // 历史消息
+  void demoGetHistorMessage() async{
+    print("demoGetHistorMessage");
+
+    if (selectConversationInfo == null) {
+      setState(() {
+        _result = "请选着需要操作的会话";
+      });
+      return;
+    }
+
+    setState(() {
+      _loading = true;
+    });
+
+    selectConversationInfo.getHistoryMessages(from: 0, limit: 20).then((msgList){
+
+      for(JMNormalMessage msg in msgList) {
+        print("get conversation history msg :   ${msg.toJson()}");
+      }
+
+      setState(() {
+        _loading = false;
+        _result = "【消息列表】${msgList.toString()}";
+      });
+    });
+  }
+}
+
---
+++ example/lib/group_manage_view.dart
@@ -0,0 +1,375 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:jmessage_flutter/jmessage_flutter.dart';
+import 'package:jmessage_flutter_example/main.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+
+
+class GroupManageView extends StatefulWidget {
+
+  @override
+  _GroupManageViewState createState() => _GroupManageViewState();
+}
+
+class _GroupManageViewState extends State<GroupManageView> {
+
+  List<dynamic> dataList = [];
+  final _commonFont = const TextStyle(fontSize: 16.0);
+  bool _loading = false;
+  String _result = "选择需要操作的群组";
+  int selectIndex = -1;
+  JMGroupInfo selectedGroupInfo;
+
+  var usernameTextEC1 = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text("群组列表",style: TextStyle(fontSize: 20)),
+        leading: IconButton(
+            icon: new Image.asset("assets/nav_close.png"),
+            onPressed: (){
+              Navigator.maybePop(context);
+            }
+        ),
+      ),
+      body: ModalProgressHUD(inAsyncCall: _loading, child: _buildContentView()),
+    );
+  }
+
+  Widget _buildContentView() {
+    return new Column(
+      children: <Widget>[
+        new Container(
+          height: 100,
+          child: new Row(
+            children: <Widget>[
+              Expanded(
+                  child: new Container(
+                    margin: EdgeInsets.fromLTRB(5, 5, 5, 0),
+                    color: Colors.brown,
+                    child: Text(_result),
+                    height: double.infinity,
+                  ),
+                flex: 2,
+              ),
+              Expanded(
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    new CustomButton(title: "私有群列表", onPressed: demoGetGidList),
+                    new CustomButton(title: "公开群列表", onPressed: demoGetPublicGroupInfos),
+                  ],
+                ),
+                flex: 1,
+              ),
+            ],
+          ),
+        ),
+        new CustomTextField(hintText: "请输入username/group name", controller: usernameTextEC1),
+        new Row(
+          children: <Widget>[
+            new Text(" "),
+            new CustomButton(title: "成员列表", onPressed: demoGetMemberlist),
+            new Text(" "),
+            new CustomButton(title: "加入成员", onPressed: demoAddMember),
+            new Text(" "),
+            new CustomButton(title: "移除成员", onPressed: demoRemoveMember),
+            new Text(" "),
+            new CustomButton(title: "申请加入", onPressed: demoApplyJoinGroup),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Text(" "),
+            new CustomButton(title: "创建公开群组", onPressed: (){
+              demoCreateGroup(JMGroupType.public);
+            }),
+            new Text(" "),
+            new CustomButton(title: "创建私有群组", onPressed: (){
+              demoCreateGroup(JMGroupType.private);
+            }),
+            new Text(" "),
+            new CustomButton(title: "创建群聊会话", onPressed: demoCreatConversation),
+          ],
+        ),
+        Expanded(
+          child: new Container(
+            color: Colors.grey,
+            margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+            child: new ListView.builder(
+                itemCount: dataList.length*2,
+                itemBuilder: (context, i){
+                  if (i.isOdd) return new Divider();
+                  final index = i ~/2;
+                  return _buildRow(dataList[index], index);
+                }
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildRow(dynamic object, int index) {
+    String title = "";
+    String subTitle = "";
+    if (object is String) {
+      title = "【私有群】gid: " + object;
+    }else if (object is JMGroupInfo) {
+      title = "【公开群】name: " + object.name ;
+      subTitle = " gid: " + object.id  ;
+    }
+
+    return new Container(
+      height: 60,
+      child: new ListTile(
+        //dense: true,
+        //leading: CircleAvatar(backgroundImage: NetworkImage(url)),
+        //trailing: Icon(Icons.keyboard_arrow_right),
+        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),//设置内容边距，默认是 16
+        title: new Text(title),
+        subtitle: new Text(subTitle, style: TextStyle(fontSize: 14)),
+        selected: selectIndex==index,
+        onTap: () {
+          print("点击了第【${index+1}】行");
+          String gid ;
+          if (object is String) {
+            gid = object;
+          }else if (object is JMGroupInfo) {
+            gid = object.id;
+          }
+          demoGetGroupInfo(gid);
+          setState(() {
+            selectIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  void demoGetGroupInfo(String gid) async {
+    print("demoGetGroupInfo gid = $gid");
+
+    setState(() {
+      _loading = true;
+    });
+
+    JMGroupInfo groupInfo = await jmessage.getGroupInfo(id: gid);
+    print("群信息：${groupInfo.toJson()}");
+
+    selectedGroupInfo = groupInfo;
+
+    setState(() {
+      _loading = false;
+      _result = "【选择的群组信息】\n ${groupInfo.toJson()}";
+    });
+  }
+
+  void demoGetMemberlist() async {
+    print("demoGetMemberlist ");
+
+    if (selectedGroupInfo == null) {
+      setState(() {
+        _result = "请选着需要操作的群组";
+      });
+      return;
+    }
+
+    setState(() {
+      _loading = true;
+    });
+
+    String gid = selectedGroupInfo.id;
+    List<JMGroupMemberInfo> res = await jmessage.getGroupMembers(id: gid);
+    print("群组【gid:$gid】的群成员列表：");
+    for(JMGroupMemberInfo member in res) {
+      print("group member info :   ${member.toJson()}");
+    }
+    setState(() {
+      _loading = false;
+      _result = "【群成员信息】请查看控制台 log 输出";
+    });
+  }
+
+  void demoAddMember() async {
+    print("demoAddMember ");
+
+    if (usernameTextEC1.text == null) {
+      setState(() {
+        _loading = false;
+        _result = "【添加群成员】username 不能为空";
+      });
+      return;
+    }
+    if (selectedGroupInfo == null) {
+      setState(() {
+        _loading = false;
+        _result = "【添加群成员】请选着需要操作的群组";
+      });
+      return;
+    }
+    String gid = selectedGroupInfo.id;
+    String username = usernameTextEC1.text;
+    await jmessage.addGroupMembers(id: gid, usernameArray: [username]);
+    setState(() {
+      _loading = false;
+      _result = "【添加群成员】操作完成";
+    });
+  }
+
+  void demoRemoveMember() async {
+    print("demoRemoveMember ");
+
+    if (usernameTextEC1.text == null) {
+      setState(() {
+        _loading = false;
+        _result = "【移除群成员】username 不能为空";
+      });
+      return;
+    }
+    if (selectedGroupInfo == null) {
+      setState(() {
+        _loading = false;
+        _result = "【移除群成员】请选着需要操作的群组";
+      });
+      return;
+    }
+    String gid = selectedGroupInfo.id;
+    String username = usernameTextEC1.text;
+    await jmessage.removeGroupMembers(id: gid, usernames: [username]);
+
+    setState(() {
+      _loading = false;
+      _result = "【移除群成员】操作完成";
+    });
+  }
+
+  void demoApplyJoinGroup() async {
+    print("demoApplyJoinGroup");
+
+    setState(() {
+      _loading = true;
+    });
+
+    if (selectedGroupInfo == null) {
+      setState(() {
+        _loading = false;
+        _result = "【申请加入群组】请选着需要操作的群组";
+      });
+      return;
+    }
+
+    if (selectedGroupInfo.groupType == JMGroupType.private) {
+      setState(() {
+        _loading = false;
+        _result = "【申请加入群组】该群为私有群，可直接加入";
+      });
+      return ;
+    }
+
+    String gid = selectedGroupInfo.id;
+    await jmessage.applyJoinGroup(groupId: gid);
+
+    setState(() {
+      _loading = false;
+      _result = "【申请加入群组】操作完成";
+    });
+  }
+
+  /// 获取 我的群列表
+  void demoGetGidList() async {
+    print("demoGetGroupList");
+
+    reset();
+
+    setState(() {
+      _loading = true;
+    });
+    List<String>list = await jmessage.getGroupIds();
+    setState(() {
+      _loading = false;
+      dataList = list;
+    });
+
+  }
+
+  /// 获取公开群列表
+  void demoGetPublicGroupInfos () async {
+    print("demoGetPublicGroupInfos");
+
+    reset();
+    setState(() {
+      _loading = true;
+    });
+
+    JMUserInfo userInfo = await jmessage.getMyInfo();
+    String appkey = userInfo.appKey;
+
+    List<JMGroupInfo> groupList = await jmessage.getPublicGroupInfos(appKey: appkey, start: 0, count: 20);
+    setState(() {
+      _loading = false;
+      dataList = groupList;
+    });
+  }
+
+  void demoCreateGroup(JMGroupType type) async {
+    print("demoCreateGroup" + usernameTextEC1.text);
+
+    setState(() {
+      _loading = true;
+    });
+
+    if (usernameTextEC1.text == null || usernameTextEC1.text == "") {
+      setState(() {
+        _loading = false;
+        _result = "【创建公开群】group name 不能为空";
+      });
+      return;
+    }
+    String name = usernameTextEC1.text;
+    String groupIdString = await jmessage.createGroup(groupType: type, name: name, desc: "$name-的群描述信息");
+    setState(() {
+      _loading = false;
+      _result = "【创建群组】创建成功，gid = $groupIdString";
+    });
+  }
+
+  void demoCreatConversation() async {
+    print("demoCreatConversation");
+
+    setState(() {
+      _loading = true;
+    });
+
+    if (selectedGroupInfo == null) {
+      setState(() {
+        _loading = false;
+        _result = "【创建群聊会话】请选着需要操作的群组";
+      });
+      return;
+    }
+
+    String gid = selectedGroupInfo.id;
+    JMGroup group = JMGroup.fromJson({"groupId":gid});
+    JMConversationInfo conversationInfo = await jmessage.createConversation(target: group);
+
+    setState(() {
+      _loading = false;
+      _result = "【创建群聊会话】 $conversationInfo";
+    });
+  }
+
+  void reset(){
+    setState(() {
+      _result = "选择需要操作的群组";
+      selectIndex = -1;
+      selectedGroupInfo = null;
+    });
+  }
+
+}
+
